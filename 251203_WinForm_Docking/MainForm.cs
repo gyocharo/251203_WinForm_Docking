26f3c0171e770c7a72aa04f6c5313232d08c658d
jidam <jhlee@jidam.site>
2025-12-03 13:55:08 +0900

    초기 커밋.

---
+++ 251203_WinForm_Docking/MainForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace _251203_WinForm_Docking
+{
+    public partial class MainForm : Form
+    {
+        private static DockPanel _dockPanel;
+        public MainForm()
+        {
+            InitializeComponent();
+
+            _dockPanel = new DockPanel()
+            {
+                Dock = DockStyle.Fill
+            };
+            Controls.Add(_dockPanel);
+
+            _dockPanel.Theme = new VS2015BlueTheme();
+
+            LoadDockingWindows();
+        }
+
+        private void LoadDockingWindows()
+        {
+            CameraForm cameraForm = new CameraForm();
+            cameraForm.Show(_dockPanel, DockState.Document);
+
+            ResultForm resultForm = new ResultForm();
+            resultForm.Show(cameraForm.Pane, DockAlignment.Bottom, 0.3);
+
+            PropertiesForm propForm = new PropertiesForm();
+            propForm.Show(_dockPanel, DockState.DockRight);
+
+            StatisticForm statisticForm = new StatisticForm();
+            statisticForm.Show(_dockPanel, DockState.DockRight);
+
+            LogForm logForm = new LogForm();
+            logForm.Show(propForm.Pane, DockAlignment.Bottom, 0.4);
+
+        }
+    }
+}
