2c061fab4e93609dae6345d0eb4f754e151e2dd5
Yeon <yeonsujin97@gmail.com>
2026-01-28 16:44:20 +0900

    AIModuleProp.Designer 수정

--- PureGate/Property/AIModuleProp.Designer.cs
+++ PureGate/Property/AIModuleProp.Designer.cs
@@ -63,6 +63,40 @@ namespace PureGate.Property
             this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
+            // txtAIModelPath
+            // 
+            this.txtAIModelPath.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtAIModelPath.Location = new System.Drawing.Point(4, 4);
+            this.txtAIModelPath.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAIModelPath.Name = "txtAIModelPath";
+            this.txtAIModelPath.ReadOnly = true;
+            this.txtAIModelPath.Size = new System.Drawing.Size(666, 28);
+            this.txtAIModelPath.TabIndex = 6;
+            // 
+            // btnLoadModel
+            // 
+            this.btnLoadModel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnLoadModel.Location = new System.Drawing.Point(521, 217);
+            this.btnLoadModel.Margin = new System.Windows.Forms.Padding(5);
+            this.btnLoadModel.Name = "btnLoadModel";
+            this.btnLoadModel.Size = new System.Drawing.Size(148, 49);
+            this.btnLoadModel.TabIndex = 8;
+            this.btnLoadModel.Text = "모델 로딩";
+            this.btnLoadModel.UseVisualStyleBackColor = true;
+            this.btnLoadModel.Click += new System.EventHandler(this.btnLoadModel_Click);
+            // 
+            // btnSelAIModel
+            // 
+            this.btnSelAIModel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnSelAIModel.Location = new System.Drawing.Point(551, 158);
+            this.btnSelAIModel.Margin = new System.Windows.Forms.Padding(5);
+            this.btnSelAIModel.Name = "btnSelAIModel";
+            this.btnSelAIModel.Size = new System.Drawing.Size(118, 49);
+            this.btnSelAIModel.TabIndex = 7;
+            this.btnSelAIModel.Text = "AI모델 선택";
+            this.btnSelAIModel.UseVisualStyleBackColor = true;
+            this.btnSelAIModel.Click += new System.EventHandler(this.btnSelAIModel_Click);
+            // 
             // btnInspAI
             // 
             this.btnInspAI.Dock = System.Windows.Forms.DockStyle.Right;
@@ -74,48 +108,14 @@ namespace PureGate.Property
             this.btnInspAI.UseVisualStyleBackColor = true;
             this.btnInspAI.Click += new System.EventHandler(this.btnInspAI_Click);
             // 
-            // txtAIModelPath
-            // 
-            this.txtAIModelPath.Dock = System.Windows.Forms.DockStyle.Top;
-            this.txtAIModelPath.Location = new System.Drawing.Point(3, 3);
-            this.txtAIModelPath.Name = "txtAIModelPath";
-            this.txtAIModelPath.ReadOnly = true;
-            this.txtAIModelPath.Size = new System.Drawing.Size(533, 25);
-            this.txtAIModelPath.TabIndex = 6;
-            // 
-            // btnLoadModel
-            // 
-            this.btnLoadModel.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnLoadModel.Location = new System.Drawing.Point(272, 179);
-            this.btnLoadModel.Margin = new System.Windows.Forms.Padding(4);
-            this.btnLoadModel.Location = new System.Drawing.Point(442, 113);
-            this.btnLoadModel.Name = "btnLoadModel";
-            this.btnLoadModel.Size = new System.Drawing.Size(118, 64);
-            this.btnLoadModel.TabIndex = 8;
-            this.btnLoadModel.Text = "모델 로딩";
-            this.btnLoadModel.UseVisualStyleBackColor = true;
-            this.btnLoadModel.Click += new System.EventHandler(this.btnLoadModel_Click);
-            // 
-            // btnSelAIModel
-            // 
-            this.btnSelAIModel.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnSelAIModel.Location = new System.Drawing.Point(272, 104);
-            this.btnSelAIModel.Margin = new System.Windows.Forms.Padding(4);
-            this.btnSelAIModel.Name = "btnSelAIModel";
-            this.btnSelAIModel.Size = new System.Drawing.Size(94, 43);
-            this.btnSelAIModel.TabIndex = 7;
-            this.btnSelAIModel.Text = "AI모델 선택";
-            this.btnSelAIModel.UseVisualStyleBackColor = true;
-            this.btnSelAIModel.Click += new System.EventHandler(this.btnSelAIModel_Click);
-            // 
             // Txt_ModuleInfo
             // 
             this.Txt_ModuleInfo.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Txt_ModuleInfo.Location = new System.Drawing.Point(4, 275);
-            this.Txt_ModuleInfo.Margin = new System.Windows.Forms.Padding(4);
+            this.Txt_ModuleInfo.Location = new System.Drawing.Point(5, 392);
+            this.Txt_ModuleInfo.Margin = new System.Windows.Forms.Padding(5);
             this.Txt_ModuleInfo.Multiline = true;
             this.Txt_ModuleInfo.Name = "Txt_ModuleInfo";
-            this.Txt_ModuleInfo.Size = new System.Drawing.Size(533, 17);
+            this.Txt_ModuleInfo.Size = new System.Drawing.Size(664, 41);
             this.Txt_ModuleInfo.TabIndex = 27;
             // 
             // lv_ClassInfos
@@ -124,10 +124,11 @@ namespace PureGate.Property
             this.lv_ClassInfos.GridLines = true;
             this.lv_ClassInfos.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
             this.lv_ClassInfos.HideSelection = false;
-            this.lv_ClassInfos.Location = new System.Drawing.Point(3, 90);
+            this.lv_ClassInfos.Location = new System.Drawing.Point(4, 125);
+            this.lv_ClassInfos.Margin = new System.Windows.Forms.Padding(4);
             this.lv_ClassInfos.MultiSelect = false;
             this.lv_ClassInfos.Name = "lv_ClassInfos";
-            this.lv_ClassInfos.Size = new System.Drawing.Size(533, 117);
+            this.lv_ClassInfos.Size = new System.Drawing.Size(666, 232);
             this.lv_ClassInfos.TabIndex = 25;
             this.lv_ClassInfos.UseCompatibleStateImageBehavior = false;
             this.lv_ClassInfos.View = System.Windows.Forms.View.Details;
@@ -136,10 +137,11 @@ namespace PureGate.Property
             // 
             this.lbx_ModelInformation.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lbx_ModelInformation.FormattingEnabled = true;
-            this.lbx_ModelInformation.ItemHeight = 15;
-            this.lbx_ModelInformation.Location = new System.Drawing.Point(3, 19);
+            this.lbx_ModelInformation.ItemHeight = 18;
+            this.lbx_ModelInformation.Location = new System.Drawing.Point(4, 26);
+            this.lbx_ModelInformation.Margin = new System.Windows.Forms.Padding(4);
             this.lbx_ModelInformation.Name = "lbx_ModelInformation";
-            this.lbx_ModelInformation.Size = new System.Drawing.Size(533, 65);
+            this.lbx_ModelInformation.Size = new System.Drawing.Size(666, 91);
             this.lbx_ModelInformation.TabIndex = 24;
             // 
             // Lbl_ModuleInfo
@@ -147,9 +149,10 @@ namespace PureGate.Property
             this.Lbl_ModuleInfo.AutoSize = true;
             this.Lbl_ModuleInfo.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Lbl_ModuleInfo.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.Lbl_ModuleInfo.Location = new System.Drawing.Point(3, 210);
+            this.Lbl_ModuleInfo.Location = new System.Drawing.Point(4, 361);
+            this.Lbl_ModuleInfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Lbl_ModuleInfo.Name = "Lbl_ModuleInfo";
-            this.Lbl_ModuleInfo.Size = new System.Drawing.Size(386, 19);
+            this.Lbl_ModuleInfo.Size = new System.Drawing.Size(666, 26);
             this.Lbl_ModuleInfo.TabIndex = 26;
             this.Lbl_ModuleInfo.Text = "모듈 정보";
             // 
@@ -158,9 +161,10 @@ namespace PureGate.Property
             this.label24.AutoSize = true;
             this.label24.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label24.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label24.Location = new System.Drawing.Point(3, 0);
+            this.label24.Location = new System.Drawing.Point(4, 0);
+            this.label24.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label24.Name = "label24";
-            this.label24.Size = new System.Drawing.Size(386, 19);
+            this.label24.Size = new System.Drawing.Size(666, 22);
             this.label24.TabIndex = 20;
             this.label24.Text = "모델 정보";
             // 
@@ -180,10 +184,10 @@ namespace PureGate.Property
             this.tableLayoutPanel3.RowCount = 5;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.24324F));
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 81.75676F));
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 123F));
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 22F));
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 22F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(539, 255);
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 240F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(674, 438);
             this.tableLayoutPanel3.TabIndex = 19;
             // 
             // tableLayoutPanel2
@@ -202,11 +206,11 @@ namespace PureGate.Property
             this.tableLayoutPanel2.RowCount = 5;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.58855F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.41145F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 49F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 49F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 48F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 67F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(539, 275);
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 59F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 59F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 88F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(674, 367);
             this.tableLayoutPanel2.TabIndex = 20;
             // 
             // cbAIModelType
@@ -214,10 +218,10 @@ namespace PureGate.Property
             this.cbAIModelType.Dock = System.Windows.Forms.DockStyle.Right;
             this.cbAIModelType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbAIModelType.FormattingEnabled = true;
-            this.cbAIModelType.Location = new System.Drawing.Point(4, 57);
-            this.cbAIModelType.Margin = new System.Windows.Forms.Padding(4);
+            this.cbAIModelType.Location = new System.Drawing.Point(279, 85);
+            this.cbAIModelType.Margin = new System.Windows.Forms.Padding(5);
             this.cbAIModelType.Name = "cbAIModelType";
-            this.cbAIModelType.Size = new System.Drawing.Size(386, 26);
+            this.cbAIModelType.Size = new System.Drawing.Size(390, 26);
             this.cbAIModelType.TabIndex = 5;
             this.cbAIModelType.SelectedIndexChanged += new System.EventHandler(this.cbAIModelType_SelectedIndexChanged);
             // 
@@ -228,64 +232,57 @@ namespace PureGate.Property
             this.panel1.Controls.Add(this.txtMinArea);
             this.panel1.Controls.Add(this.dasda);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(2, 209);
+            this.panel1.Location = new System.Drawing.Point(2, 273);
             this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(535, 64);
+            this.panel1.Size = new System.Drawing.Size(670, 85);
             this.panel1.TabIndex = 23;
             // 
             // lblAreaFilter
             // 
             this.lblAreaFilter.AutoSize = true;
-            this.lblAreaFilter.Location = new System.Drawing.Point(12, 38);
+            this.lblAreaFilter.Location = new System.Drawing.Point(15, 46);
             this.lblAreaFilter.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblAreaFilter.Name = "lblAreaFilter";
-            this.lblAreaFilter.Size = new System.Drawing.Size(161, 15);
+            this.lblAreaFilter.Size = new System.Drawing.Size(238, 18);
             this.lblAreaFilter.TabIndex = 24;
             this.lblAreaFilter.Text = "영역 필터 (최소값 ~ 최대값)";
             // 
             // txtMaxArea
             // 
-            this.txtMaxArea.Location = new System.Drawing.Point(4, 3);
+            this.txtMaxArea.Location = new System.Drawing.Point(5, 4);
+            this.txtMaxArea.Margin = new System.Windows.Forms.Padding(4);
             this.txtMaxArea.Name = "txtMaxArea";
-            this.txtMaxArea.Size = new System.Drawing.Size(70, 25);
+            this.txtMaxArea.Size = new System.Drawing.Size(86, 28);
             this.txtMaxArea.TabIndex = 14;
             // 
             // txtMinArea
             // 
-            this.txtMinArea.Location = new System.Drawing.Point(103, 3);
+            this.txtMinArea.Location = new System.Drawing.Point(129, 4);
+            this.txtMinArea.Margin = new System.Windows.Forms.Padding(4);
             this.txtMinArea.Name = "txtMinArea";
-            this.txtMinArea.Size = new System.Drawing.Size(70, 25);
+            this.txtMinArea.Size = new System.Drawing.Size(86, 28);
             this.txtMinArea.TabIndex = 15;
             // 
             // dasda
             // 
             this.dasda.AutoSize = true;
-            this.dasda.Location = new System.Drawing.Point(79, 6);
+            this.dasda.Location = new System.Drawing.Point(99, 7);
+            this.dasda.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.dasda.Name = "dasda";
-            this.dasda.Size = new System.Drawing.Size(18, 15);
+            this.dasda.Size = new System.Drawing.Size(22, 18);
             this.dasda.TabIndex = 17;
             this.dasda.Text = "~";
-            // 
-            // cbAIModelType
-            // 
-            this.cbAIModelType.Dock = System.Windows.Forms.DockStyle.Right;
-            this.cbAIModelType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbAIModelType.FormattingEnabled = true;
-            this.cbAIModelType.Location = new System.Drawing.Point(223, 35);
-            this.cbAIModelType.Name = "cbAIModelType";
-            this.cbAIModelType.Size = new System.Drawing.Size(313, 23);
-            this.cbAIModelType.TabIndex = 5;
-            this.cbAIModelType.SelectedIndexChanged += new System.EventHandler(this.cbAIModelType_SelectedIndexChanged);
             // 
             // panel2
             // 
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel2.Controls.Add(this.tableLayoutPanel3);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 416);
+            this.panel2.Location = new System.Drawing.Point(0, 405);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(541, 257);
+            this.panel2.Size = new System.Drawing.Size(676, 440);
             this.panel2.TabIndex = 21;
             // 
             // panel3
@@ -296,17 +293,18 @@ namespace PureGate.Property
             this.panel3.Location = new System.Drawing.Point(0, 0);
             this.panel3.Margin = new System.Windows.Forms.Padding(2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(541, 277);
+            this.panel3.Size = new System.Drawing.Size(676, 369);
             this.panel3.TabIndex = 22;
             // 
             // AIModuleProp
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "AIModuleProp";
-            this.Size = new System.Drawing.Size(541, 673);
+            this.Size = new System.Drawing.Size(676, 808);
             this.tableLayoutPanel3.ResumeLayout(false);
             this.tableLayoutPanel3.PerformLayout();
             this.tableLayoutPanel2.ResumeLayout(false);
