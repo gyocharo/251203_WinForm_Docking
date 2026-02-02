f04af7daa32c156de45a31baf6ad18b99026199b
sjh990827 <user@DESKTOP-AI5IPQK>
2026-02-02 14:53:36 +0900

    통계 저장 폴더 열기 버튼 위치 수정

--- PureGate/SummaryForm.Designer.cs
+++ PureGate/SummaryForm.Designer.cs
@@ -28,12 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea11 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend11 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea12 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend12 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.NGResultChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.OkNgChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.btnOpenSaveFolder = new System.Windows.Forms.Button();
@@ -43,17 +43,17 @@
             // 
             // NGResultChart
             // 
-            chartArea1.Name = "ChartArea1";
-            this.NGResultChart.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.NGResultChart.Legends.Add(legend1);
+            chartArea11.Name = "ChartArea1";
+            this.NGResultChart.ChartAreas.Add(chartArea11);
+            legend11.Name = "Legend1";
+            this.NGResultChart.Legends.Add(legend11);
             this.NGResultChart.Location = new System.Drawing.Point(406, 239);
             this.NGResultChart.Name = "NGResultChart";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.NGResultChart.Series.Add(series1);
+            series11.ChartArea = "ChartArea1";
+            series11.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
+            series11.Legend = "Legend1";
+            series11.Name = "Series1";
+            this.NGResultChart.Series.Add(series11);
             this.NGResultChart.Size = new System.Drawing.Size(394, 211);
             this.NGResultChart.TabIndex = 0;
             this.NGResultChart.TabStop = false;
@@ -61,27 +61,27 @@
             // 
             // OkNgChart
             // 
-            chartArea2.Name = "ChartArea1";
-            this.OkNgChart.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.OkNgChart.Legends.Add(legend2);
+            chartArea12.Name = "ChartArea1";
+            this.OkNgChart.ChartAreas.Add(chartArea12);
+            legend12.Name = "Legend1";
+            this.OkNgChart.Legends.Add(legend12);
             this.OkNgChart.Location = new System.Drawing.Point(6, 239);
             this.OkNgChart.Name = "OkNgChart";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.OkNgChart.Series.Add(series2);
+            series12.ChartArea = "ChartArea1";
+            series12.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
+            series12.Legend = "Legend1";
+            series12.Name = "Series1";
+            this.OkNgChart.Series.Add(series12);
             this.OkNgChart.Size = new System.Drawing.Size(394, 211);
             this.OkNgChart.TabIndex = 1;
             this.OkNgChart.Text = "chart1";
             // 
             // btnOpenSaveFolder
             // 
-            this.btnOpenSaveFolder.Font = new System.Drawing.Font("굴림", 7F);
-            this.btnOpenSaveFolder.Location = new System.Drawing.Point(623, 171);
+            this.btnOpenSaveFolder.Font = new System.Drawing.Font("굴림", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnOpenSaveFolder.Location = new System.Drawing.Point(370, 46);
             this.btnOpenSaveFolder.Name = "btnOpenSaveFolder";
-            this.btnOpenSaveFolder.Size = new System.Drawing.Size(159, 40);
+            this.btnOpenSaveFolder.Size = new System.Drawing.Size(136, 55);
             this.btnOpenSaveFolder.TabIndex = 2;
             this.btnOpenSaveFolder.TabStop = false;
             this.btnOpenSaveFolder.Text = "통계 저장 폴더 열기";
